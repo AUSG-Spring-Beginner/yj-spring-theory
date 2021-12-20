bd84f7c749262118c74abe0ea31be6671e8c1110
YuJin <lyuashley@gmail.com>
2021-12-20 17:49:30 +0900

    chapter 08-5까지 완료

--- yujin-project/sp5-chap07/src/main/java/aspect/ExeTimeAspect.java
+++ yujin-project/sp5-chap07/src/main/java/aspect/ExeTimeAspect.java
@@ -15,11 +15,12 @@ public class ExeTimeAspect {
     @Pointcut("execution(public * chap07..*(..))")
     private void publicTarget(){ }
 
+    // 여기서의 공통 기능: 실행 시간 측정 / 핵심 기능: 팩토리얼
     @Around("publicTarget()")
     public Object measure(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.nanoTime();
         try {
-            Object result = joinPoint.proceed();
+            Object result = joinPoint.proceed(); // 실제 대상 객체 메서드 호출해서 핵심 기능 수행하도록
             return result;
         } finally {
             long finish = System.nanoTime();
